929c2f6328b95ce87734a50bd47481f94f056e88
gonzalohernandez1293 <goncho_hdez@hotmail.com>
2016-04-26 12:01:41 +0200

    Added files via upload

--- GTableV2.js
+++ GTableV2.js
@@ -84,7 +84,17 @@
             if (evento != null)
                fila.dblclick(evento);
          }
-		 var funcionOrdenacion = methods.sortTable;
+		 var funcionOrdenacion = function(property){
+		var sortOrder = 1;
+		if(property[0] === "-") {
+			sortOrder = -1;
+			property = property.substr(1);
+		}
+		return function (a,b) {
+			var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+			return result * sortOrder;
+		}
+	  };
 		 var dataEvent= [this.variables.datos,funcionOrdenacion];
 		 this.find('thead tr th').bind('click',dataEvent,function (e){ //Ordenable
 		   	console.log(e.dataEvent[0]);
